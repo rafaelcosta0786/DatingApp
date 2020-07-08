67638d0ebd484c0263a6d6a247b558e08daa9b2f
Rafael Costa <rafael.costa@cognizant.com>
2020-07-08 11:36:25 -0300

    Final da seção 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CarroComponent } from './carro/carro.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
-  declarations: [AppComponent, CarroComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, CarroComponent, NavComponent, HomeComponent, RegistroComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ DatingApp-SPA/src/app/registro/registro.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css'],
+})
+export class RegistroComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('cadastro sucesso');
+    }, error => {
+      console.log('cadastro falha');
+    })
+  }
+
+  cancel() {
+    this.cancelRegister.emit(true);
+  }
+}
